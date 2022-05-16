ac61a3f0e3b90a6999b5fbd229b5ff0d73fedb19
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-16 18:22:48 +0530

    Add files via upload

---
+++ sep_end_combo.py
@@ -0,0 +1,21 @@
+"""
+Name: Karthik
+Date: 16th May 2022
+Day: Monday
+Time: 6:00-7:00PM
+File Description:  Separate_End_Combination
+"""
+# Separate can only be done using ,
+# print(1,2,3,4,sep='-')
+# print('hello','3.1.4',sep='=')
+
+#end
+# print('hi\n'*3, end="")
+# print("My name is",end=",")
+# print("karthik")
+
+#combination of end and separate 
+print(1,2,3,4,5,sep='-',end=",")
+print(6,7,8)
+print(9,10)
+
